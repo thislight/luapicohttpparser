89842338b05b7b5e62a39a43fc73a28d69e1d475
thisLight <l1589002388@gmail.com>
2020-09-06 10:14:27 +0800

    update example for lphr.c.paarrse_response

--- example/lphrc.lua
+++ example/lphrc.lua
@@ -1,9 +1,10 @@
 local lphrc = require "lphr.c"
 
-RAW_REQUEST = {
+print("----parse_request----")
+
+local RAW_REQUEST = {
     "GET /tamse HTTP/1.0\r\n",
     "Host: example.org\r\n",
-    "\r\n",
     "\r\n",
 }
 
@@ -20,3 +21,29 @@ print("minor_version",data.minor_version)
 print("method",data.method)
 print("headers[1]", data.headers[1][1], data.headers[1][2])
 print("path", data.path)
+print("----------")
+
+print("----parse_response----")
+
+local RAW_RESPONSE = {
+    "HTTP/1.1 200 OK\r\n",
+    "Content-Length: 3059\r\n",
+    "Server: GWS/2.0\r\n",
+    "Connection: keep-alive\r\n",
+    "\r\n",
+}
+
+local data = {}
+local buffer = ""
+for _, v in ipairs(RAW_RESPONSE) do
+    buffer = buffer..v
+    if lphrc.parse_response(buffer, data) ~= -2 then
+        break
+    end
+end
+
+print("minor_version",data.minor_version)
+print("status", data.status)
+print("message", data.message)
+print("#headers", #data.headers)
+print("----------")
